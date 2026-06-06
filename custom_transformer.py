a256829c956086005a3edf20f1082e30dfa5b47a
Stephen Blum <blum.stephen@gmail.com>
2026-06-06 10:25:53 -0700

    almost there!

--- custom_transformer.py
+++ custom_transformer.py
@@ -47,18 +47,15 @@ class StephenFormer(torch.nn.Module):
         self.output_projection = torch.nn.Linear(dims, len(dictionary))
 
     def attention(self, query, key, value):
-        print(query.shape)
-        print(query.shape[0])
-        #B = query.shape
-        #query = query.view(B
-        #key = key
-        #value = value
         ## TODO Multi-headed attention
         out = query @ key.transpose(1,2)
         out = out / torch.sqrt(torch.tensor(self.dims))
+        #out = torch.softmax(out, dim=-1)
         out = self.softmax(out)
         out = self.dropout(out)
         out = out @ value
+        print('attention')
+        print(out.shape)
         return out
 
     def forward(self, inputs):
@@ -68,11 +65,16 @@ class StephenFormer(torch.nn.Module):
         key   = self.key_projection(inputs)
         value = self.value_projection(inputs)
         out = self.attention(query, key, value)
+        print('forward')
+        print(out.shape)
         out = self.norm(inputs + out)
+        print(out.shape)
         out = self.feedforward(out)
+        print(out.shape)
         out = self.norm(inputs + out)
+        print(out.shape)
         out = self.output_projection(out)
-        out = self.softmax(out)
+        print(out.shape)
         return out
 
 
@@ -95,7 +97,8 @@ class PositionalEncoding(torch.nn.Module):
         self.register_buffer("pe", pe)
 
     def forward(self, x):
-        return x + self.pe[:x.shape[0],:]
+	### 32,20, hidden
+        return x + self.pe[:x.shape[0],:].unsqueeze(0)
 
 class Dictionary(torch.nn.Module):
     def __init__(self, corpus):
@@ -137,17 +140,18 @@ class Dictionary(torch.nn.Module):
         #return re.sub(self.norm, '', words.lower())
         #return words
         
-    def tokenize(self, batches):
+    def tokenize(self, batch_size):
         tokens = [
             [self.dictionary[words]
                 for words in self.normalize(phrases)
-            ] for phrases in batches
+            ] for phrases in batch_size
         ]
         return torch.Tensor(tokens).to(torch.long)
 
     def one_hot(self, words):
         tokens = self.tokenize(words)
-        return torch.nn.functional.one_hot(tokens)#.to(torch.float)
+        return torch.nn.functional.one_hot(tokens).to(torch.float)
+
         
 class Transformer(torch.nn.Module):
     def __init__(self, dictionary):
@@ -181,8 +185,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001)
 criterion = torch.nn.CrossEntropyLoss(ignore_index=0)
 #criterion = torch.nn.NLLLoss(ignore_index=0)
 epochs = 1
-batches = 10
-batch_size = 20
+batch_size = 32
 
 ## TODO rewrite for new data type
 training_set = {
@@ -191,39 +194,49 @@ training_set = {
     'len'      : len(training_data),
     'window'   : 20, ## tokens
 }
+## TODO Problem - convert to a window target, not just one value
 def batch_prepare():
     window = training_set['window']
     for position in range(training_set['len'] - window - 1):
         segment = training_data[position:position+window]
-        target = training_data[position+window]
+        target = training_data[position+1:position+window+1]
         training_set['features'].append(segment)
         training_set['labels'].append(target)
 
 ## TODO problem with out of range index
+## TODO convert to a generator with yield
+##    ChatGPT has a lot of goblins and gremlins token output due to training
+## Don't delete this -> CodeLangtonsEntropy -> Goblins and Gremlins <-
+## 
 def get_batch():
-    indexes = torch.randint(0, training_set['len'], (batch_size,))
-    features = [training_set['features'][index] for index in indexes]
-    labels = [training_set['labels'][index] for index in indexes]
-    return features, labels
+    samples = len(training_set['features'])
+    indexes = torch.arange(0, samples)[torch.randperm(samples)]
+    seen = set()
+    for batch in range(len(indexes) // batch_size):
+        start = batch * batch_size
+        end = (batch+1) * batch_size
+        seen.add(indexes[start:end])
+        features = [training_set['features'][index] for index in indexes[start:end]]
+        labels = [training_set['labels'][index] for index in indexes[start:end]]
+        yield features, labels
 
 def train():
     for epoch in range(epochs):
-        for batch in range(batches):
-            features, target = get_batch()
+        for features, targets in get_batch():
             output = model(features)
-            targets = dictionary.one_hot(target)#.reshape(-1)
             output = output#.reshape(-1, len(dictionary))
-            #targets = dictionary.tokenize(target).reshape(-1)
-            #targets = dictionary.one_hot(targets)
+            #targets = dictionary.tokenize(targets).unsqueeze(2)#[:,:,None]#.reshape(-1)
+            targets = dictionary.one_hot(targets)#.unsqueeze(2)#[:,:,None]#.reshape(-1)
+            print(targets)
             print('targets')
             #print(targets)
             print(targets.shape)
             print('output')
             #print(output)
             print(output.shape)
-            #loss = criterion(output, targets)
+            loss = criterion(output, targets)
             #loss = criterion(output.reshape(-1, len(dictionary)), targets)
-            #print('loss',loss)
+            print('loss',loss.item())
             break
             optimizer.zero_grad()
             loss.backward()
